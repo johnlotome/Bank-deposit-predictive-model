80f684c0a6bbe9b71e888d02af83ee489061401a
johnlotome <lotomej12@gmail.com>
2020-08-26 10:48:20 +0300

    structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a model to predict if a customer will subscribe to term deposit',
+    author='lotome John',
+    license='MIT',
+)
